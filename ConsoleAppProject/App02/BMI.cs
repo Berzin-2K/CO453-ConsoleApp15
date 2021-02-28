2c8eac4f8d0217ad071b3abdc96ee7507ba02f8c
Berzin-2K <72166114+Berzin-2K@users.noreply.github.com>
2021-02-28 14:33:57 +0000

    added imperial and metric measurments as choices

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -9,7 +9,24 @@ namespace ConsoleAppProject.App02
     /// </author>
     public class BMI
     {
-       
+        public const string METRIC = "Metric";
+        public const string IMPERIAL = "Imperial";
+
+        public string SelectedUnit;
+
+        public string[] MenuChoices;
+        public void SelectUnit()
+        {
+            Console.WriteLine("Please choose either");
+            Console.WriteLine("1. " + METRIC);
+            Console.WriteLine("2. " + IMPERIAL);
+
+           
+        }
+        public string GetUnit()
+        {
+            SelectedUnit = Console.ReadLine().ToUpper();
+            return SelectedUnit;
         }
     }
 }
--- ConsoleAppProject/ConsoleHelper.cs
+++ ConsoleAppProject/ConsoleHelper.cs
@@ -19,4 +19,15 @@ namespace ConsoleAppProject
             Console.WriteLine(" ==== Introduction ==== ");
             Console.WriteLine(introduction);
         }
+        public static void OutputMenu(string[] choices)
+        {
+            int choiceNo = 0;
+            foreach (string choice in choices)
+            {
+                choiceNo++;
+                Console.WriteLine(choiceNo + ". " + choice);
+                
+            }
+        }
+    }
 }
