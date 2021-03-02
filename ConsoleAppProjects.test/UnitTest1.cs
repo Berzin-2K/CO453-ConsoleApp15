6c55e3178b6bd79c66ca1f17a55cd560d32702c0
Berzin-2K <72166114+Berzin-2K@users.noreply.github.com>
2021-03-02 22:44:04 +0000

    uploaded screenshot with more test results on them

--- ConsoleAppProjects.test/UnitTest1.cs
+++ ConsoleAppProjects.test/UnitTest1.cs
@@ -21,5 +21,50 @@ namespace ConsoleAppProjects.test
             Assert.AreEqual(expectedDistance, converter.ToDistance);
 
         }
+        [TestMethod]
+        public void TestFeetToMiles()
+        {
+            DistanceConverter converter = new DistanceConverter();
+
+            converter.FromUnit = DistanceConverter.FEET;
+            converter.ToUnit = DistanceConverter.MILES;
+
+            converter.FromDistance = 1.0;
+            converter.CalculateDistance();
+
+            double expectedDistance = 0.0001893939393939394;
+
+            Assert.AreEqual(expectedDistance, converter.ToDistance);
+        }
+        [TestMethod]
+        public void TestMilestoMetres()
+        {
+            DistanceConverter converter = new DistanceConverter();
+
+            converter.FromUnit = DistanceConverter.MILES;
+            converter.ToUnit = DistanceConverter.METRES;
+
+            converter.FromDistance = 1.0;
+            converter.CalculateDistance();
+
+            double expectedDistance = 1609.34;
+
+            Assert.AreEqual(expectedDistance, converter.ToDistance);
+        }
+        [TestMethod]
+        public void TestMetrestoFeet()
+        {
+            DistanceConverter converter = new DistanceConverter();
+
+            converter.FromUnit = DistanceConverter.METRES;
+            converter.ToUnit = DistanceConverter.FEET;
+
+            converter.FromDistance = 1.0;
+            converter.CalculateDistance();
+
+            double expectedDistance = 3.28084;
+
+            Assert.AreEqual(expectedDistance, converter.ToDistance);
+        }
     }
 }
