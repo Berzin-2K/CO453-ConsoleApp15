3eaed6032b2f88932531780c45607cf4c81ce767
Berzin-2K <72166114+Berzin-2K@users.noreply.github.com>
2021-05-26 19:41:00 +0100

    Added gameview class

---
+++ ConsoleAppProject/App05/gameview.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppProject.App05
+{
+    public class GameView
+    {
+        private Game game;
+        public void StartGame()
+        {
+            SetupConsole();
+
+            ConsoleHelper.OutputHeading(" Fire-Ice-Wind game ");
+
+            if (game == null)
+            {
+                Console.Write(" Please enter your Name > ");
+                string name = Console.ReadLine();
+
+                game = new Game("Berzin");
+            }
+            game.Start();
+        }
+        private void SetupConsole()
+        {
+            Console.SetWindowSize(100, 40);
+            Console.SetBufferSize(100, 40);
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+        }
+    }
+
+}
+
